c680d81a19b20080e3156d20e6099c453c2c69ac
fatkodima <fatkodima123@gmail.com>
2022-04-20 15:54:54 +0300

    bundle gem data_checks

---
+++ lib/data_checks.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "data_checks/version"
+
+module DataChecks
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "data_checks"
+
+require "minitest/autorun"
