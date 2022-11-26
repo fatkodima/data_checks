e5ba6296685d93add343a11f1b3a7432451492a0
fatkodima <fatkodima123@gmail.com>
2022-11-26 22:27:07 +0200

    Do not prematurely load `ActiveRecord::Base`

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 ## master (unreleased)
 
+- Do not prematurely load `ActiveRecord::Base`
+
+    See https://github.com/rails/rails/issues/46567 for details.
+
 ## 0.1.0 (2022-04-21)
 
 - First release
--- lib/data_checks.rb
+++ lib/data_checks.rb
@@ -1,20 +1,24 @@
 # frozen_string_literal: true
 
-require "data_checks/check_run"
-require "data_checks/check_result"
-require "data_checks/check"
-require "data_checks/ensure_less"
-require "data_checks/ensure_more"
-require "data_checks/ensure_no"
-require "data_checks/notifiers"
-require "data_checks/config"
-require "data_checks/runner"
-require "data_checks/status_printer"
-require "data_checks/version"
+require "active_record"
 
+require "data_checks/version"
 require "data_checks/railtie" if defined?(Rails)
 
 module DataChecks
+  extend ActiveSupport::Autoload
+
+  autoload :Check
+  autoload :CheckResult
+  autoload :CheckRun
+  autoload :Config
+  autoload :EnsureLess
+  autoload :EnsureMore
+  autoload :EnsureNo
+  autoload :Notifiers
+  autoload :Runner
+  autoload :StatusPrinter
+
   class << self
     def config
       @config ||= Config.new
